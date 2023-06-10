325cf787a911ec8f5a5233ea6c02d64cf89f2f8b
Scott Holmes <scott.holmes44@gmail.com>
2023-06-10 11:06:13 -0600

    Login page - refactored grids to be divs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { Button, Card, CardActions, CardContent, FormControl, Grid, TextField } from '@mui/material'
+import { Button, Card, CardActions, CardContent, FormControl, TextField } from '@mui/material'
 
 function Login() {
   const navigate = useNavigate();
@@ -12,76 +12,57 @@ function Login() {
   const onLogin = (e: any) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+      .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         navigate("/home")
         console.log(user);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage)
-    })
+      })
   }
 
   return (
     <>
-    <Grid 
-      container 
-      direction={'column'} 
-      alignContent={'center'}
-      justifyContent={'center'}
-      sx={{ minHeight: "50vh" }}
-    >
-      <Grid 
-        container
-        alignContent={'center'}
-        justifyContent={'center'}
-      >
+      <div style={{ minHeight: "50vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Card variant="outlined" sx={{ maxWidth: 345 }}>
           <CardContent>
             <FormControl>
-              <TextField 
-                placeholder='Email' 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-                required 
-                sx={{ width: 300 }}/>
-              <TextField 
-                placeholder='Password' 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-                required />  
+              <TextField
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                sx={{ width: 300 }}
+              />
+              <TextField
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </FormControl>
           </CardContent>
           <CardActions>
             <Button variant="contained" onClick={onLogin}>Login</Button>
           </CardActions>
         </Card>
-      </Grid>
+      </div>
 
-        <Grid 
-          container 
-          direction={'row'} 
-          alignContent={'center'}
-          justifyContent={'center'}
-        >
-          <p>
-            Dont have an account? 
-          </p>
-        </Grid>
-        <Grid 
-          container 
-          direction={'row'} 
-          alignContent={'center'}
-          justifyContent={'center'}
-        >
-          <NavLink to="/signup">
-            Sign up
-          </NavLink>
-        </Grid>
-      </Grid>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <p>
+          Don't have an account?
+        </p>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <NavLink to="/signup">
+          Sign up
+        </NavLink>
+      </div>
     </>
   )
 }
