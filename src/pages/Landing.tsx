98cb5088b2231c0124c794dacd1db8314699dba0
Scott Holmes <scott.holmes44@gmail.com>
2023-06-15 12:01:56 -0600

    Landing page - styled up

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,37 +1,72 @@
-import React from 'react'
-import background from '../images/quickdraws-unsplash.jpg'
-import { Box, Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import background from "../images/quickdraws-unsplash.jpg";
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   return (
     <>
-      <Box style={{ height: '100vh', position: 'relative' }}>
+      <Box
+        style={{
+          height: "100vh",
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <div
           style={{
-            position: 'absolute',
+            position: "absolute",
             top: 0,
             left: 0,
-            width: '100%',
-            height: '100%',
+            width: "100%",
+            height: "100%",
             zIndex: -1,
             backgroundImage: `url(${background})`,
-            backgroundSize: 'cover',
-            filter: 'sepia(0.75)',
+            backgroundSize: "cover",
+            filter: "sepia(0.75)",
           }}
         />
-        <div style={{ minHeight: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <h1>Log your climbs. Track your progress</h1>
-        </div>
 
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', textAlign: 'center', color: 'white' }}>
-          <Button variant='contained' onClick={() => navigate('/login')}>Login</Button>
+        <div
+          style={{
+            backgroundColor: "#F0F0F099",
+            borderRadius: 24,
+            zIndex: 1,
+            margin: "0 auto",
+            flexDirection: "column",
+            display: "inline-flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 24,
+          }}
+        >
+          <Box style={{ display: "block", marginBottom: 24 }}>
+            <Typography variant="h2" fontFamily={"poppins"}>
+              Log your climbs
+            </Typography>
+          </Box>
+          <Box style={{ display: "block", marginBottom: 24 }}>
+            <Typography variant="h2" fontFamily={"poppins"}>
+              Track your progress
+            </Typography>
+          </Box>
+
+          <Button
+            variant="contained"
+            size="large"
+            onClick={() => navigate("/login")}
+            style={{ backgroundColor: "black", color: "white" }}
+          >
+            Login
+          </Button>
         </div>
       </Box>
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
