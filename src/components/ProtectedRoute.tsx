7f79cf85327eb2bd8d9914e387eada1a33162dfe
Scott Holmes <scott.holmes44@gmail.com>
2023-06-19 17:52:33 -0600

    ProtectedRoute - code reformatted

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import React from "react"
+import { Navigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import firebase, { auth } from "../firebase"
+import { User, onAuthStateChanged } from "firebase/auth"
+import { BehaviorSubject } from "rxjs"
 
 export type ProtectedRouteProps = {
-  authenticationPath: string;
-  outlet: JSX.Element;
-};
+  authenticationPath: string
+  outlet: JSX.Element
+}
 
 function ProtectedRoute({ authenticationPath, outlet }: ProtectedRouteProps) {
-  const isAuthenticated = !!sessionStorage.getItem("Auth Token");
+  const isAuthenticated = !!sessionStorage.getItem("Auth Token")
 
   if (isAuthenticated) {
-    return outlet;
+    return outlet
   } else {
-    toast.error("Nice try! Please login first", { toastId: "niceTry" });
+    toast.error("Nice try! Please login first", { toastId: "niceTry" })
 
-    return <Navigate to={{ pathname: authenticationPath }} />;
+    return <Navigate to={{ pathname: authenticationPath }} />
   }
 }
 
-export default ProtectedRoute;
+export default ProtectedRoute
