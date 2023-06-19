bcffd64462e447fc1b8f2c562b17ce5ebab52f90
Scott Holmes <scott.holmes44@gmail.com>
2023-06-19 17:55:02 -0600

    Moved router out of index and into App, and added a context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
-import React from "react";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Landing from "./pages/Landing";
+import React, { useState } from "react"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Landing from "./pages/Landing"
 import ProtectedRoute, {
   ProtectedRouteProps,
-} from "./components/ProtectedRoute";
-import Dashboard from "./pages/Dashboard";
-import { Routes, Route } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
+} from "./components/ProtectedRoute"
+import Dashboard from "./pages/Dashboard"
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  useNavigate,
+} from "react-router-dom"
+import "react-toastify/dist/ReactToastify.css"
+import { Context } from "./Context"
+import { CssBaseline } from "@mui/material"
+import { ToastContainer } from "react-toastify"
 
 function App() {
+  const [user, setUser] = useState("")
   const defaultProtectedRouteProps: Omit<ProtectedRouteProps, "outlet"> = {
     authenticationPath: "/signin",
-  };
+  }
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="dashboard"
-          element={
-            <ProtectedRoute
-              {...defaultProtectedRouteProps}
-              outlet={<Dashboard />}
-            />
-          }
-        />
-      </Routes>
-    </>
-  );
+    <div>
+      {/* <Context.Provider value={{ user, setUser }}> */}
+      <CssBaseline />
+      <ToastContainer position="top-center" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute
+                {...defaultProtectedRouteProps}
+                outlet={<Dashboard />}
+              />
+            }
+          />
+        </Routes>
+      </Router>
+      {/* </Context.Provider> */}
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter as Router} from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <ToastContainer position='top-center' />
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 )
