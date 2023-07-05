5ec8a87d63e4648b2c494afeecbd89a56388c67d
Scott Holmes <scott.holmes44@gmail.com>
2023-07-05 07:14:00 -0600

    GradeGraphWrapper - grades now sort properly, and a major refactor

--- src/components/GradeGraphWrapper.tsx
+++ src/components/GradeGraphWrapper.tsx
@@ -4,129 +4,108 @@ import { UserContext } from "../Context"
 import { ClimbLog, ClimbGraphData } from "../static/types"
 import { Grid } from "@mui/material"
 import GradeGraph from "./GradeGraph"
+import { INDOOR_SPORT_GRADES } from "../static/constants"
 
 const GradeGraphWrapper = () => {
   const { user } = useContext(UserContext)
   const [climbingData, setClimbingData] = useState<ClimbLog[]>([])
-
-  const [boulderData, setBoulderData] = useState<ClimbGraphData[]>([])
-  const [leadData, setLeadData] = useState<ClimbGraphData[]>([])
-  const [trData, setTrData] = useState<ClimbGraphData[]>([])
+  const [graphData, setGraphData] = useState({
+    boulderData: [] as ClimbGraphData[],
+    leadData: [] as ClimbGraphData[],
+    trData: [] as ClimbGraphData[],
+  })
 
   useEffect(() => {
-    if (!user) return
-    GetClimbsByUser(user.id).then((data) => {
-      setClimbingData(data)
-    })
+    if (user) {
+      GetClimbsByUser(user.id).then((data) => {
+        setClimbingData(data)
+      })
+    }
   }, [user])
 
   useEffect(() => {
     if (climbingData.length > 0) {
-      formatClimbingData()
+      const formattedData = formatClimbingData()
+      setGraphData(formattedData)
     }
   }, [climbingData])
 
   function formatClimbingData() {
-    const boulderGrades = new Array<string>()
     const boulderGradeAttemptMap = new Map<string, number>()
-    const boulderGraphData = new Array<ClimbGraphData>()
-
-    const leadGrades = new Array<string>()
     const leadGradeAttemptMap = new Map<string, number>()
-    const leadGraphData = new Array<ClimbGraphData>()
-
-    const trGrades = new Array<string>()
     const trGradeAttemptMap = new Map<string, number>()
-    const trGraphData = new Array<ClimbGraphData>()
 
-    // The raw data returned from firebase is a giant list of climbs
-    // Go through that list and sort the data into the appropriate buckets
     climbingData.forEach((climb) => {
-      switch (climb.ClimbType) {
-        case "Boulder":
-          addGradeData(climb, boulderGradeAttemptMap, boulderGrades)
-          break
+      const gradeAttemptMap =
+        climb.ClimbType === "Boulder"
+          ? boulderGradeAttemptMap
+          : climb.ClimbType === "Lead"
+          ? leadGradeAttemptMap
+          : trGradeAttemptMap
 
-        case "Lead":
-          addGradeData(climb, leadGradeAttemptMap, leadGrades)
-          break
-
-        case "TopRope":
-          addGradeData(climb, trGradeAttemptMap, trGrades)
-          break
-
-        default:
-          break
-      }
+      addGradeData(climb, gradeAttemptMap)
     })
 
-    // sort grades in descending order
-    boulderGrades.sort((a, b) => {
-      if (a > b) return -1
-      if (a < b) return 1
-      return 0
-    })
-    leadGrades.sort((a, b) => {
-      if (a > b) return -1
-      if (a < b) return 1
-      return 0
-    })
-    trGrades.sort((a, b) => {
-      if (a > b) return -1
-      if (a < b) return 1
-      return 0
-    })
+    // Get sorted lists of the grades in descending order
+    const boulderGrades = Array.from(
+      boulderGradeAttemptMap.keys()
+    ).sort((a, b) => b.localeCompare(a))
 
-    asssembleGraphData(boulderGrades, boulderGradeAttemptMap, boulderGraphData)
-    asssembleGraphData(leadGrades, leadGradeAttemptMap, leadGraphData)
-    asssembleGraphData(trGrades, trGradeAttemptMap, trGraphData)
-
-    setBoulderData(boulderGraphData)
-    setLeadData(leadGraphData)
-    setTrData(trGraphData)
-  }
-
-  // Helper functions
-  function addGradeData(
-    climb: ClimbLog,
-    gradeAttemptMap: Map<string, number>,
-    gradeArray: Array<string>
-  ) {
-    if (gradeAttemptMap.has(climb.Grade)) {
-      gradeAttemptMap.set(
-        climb.Grade,
-        (gradeAttemptMap.get(climb.Grade) || 0) + climb.Attempts
+    const leadGrades = Array.from(leadGradeAttemptMap.keys())
+      .sort(
+        (a, b) =>
+          INDOOR_SPORT_GRADES.indexOf(a) - INDOOR_SPORT_GRADES.indexOf(b)
       )
-    } else {
-      gradeArray.push(climb.Grade)
-      gradeAttemptMap.set(climb.Grade, climb.Attempts)
+      .reverse()
+
+    const trGrades = Array.from(trGradeAttemptMap.keys())
+      .sort(
+        (a, b) =>
+          INDOOR_SPORT_GRADES.indexOf(a) - INDOOR_SPORT_GRADES.indexOf(b)
+      )
+      .reverse()
+
+    // Assemble the data for each graph
+    const boulderGraphData = assembleGraphData(
+      boulderGrades,
+      boulderGradeAttemptMap
+    )
+    const leadGraphData = assembleGraphData(leadGrades, leadGradeAttemptMap)
+    const trGraphData = assembleGraphData(trGrades, trGradeAttemptMap)
+
+    return {
+      boulderData: boulderGraphData,
+      leadData: leadGraphData,
+      trData: trGraphData,
     }
   }
 
-  function asssembleGraphData(
-    gradesArray: Array<string>,
-    gradeAttemptMap: Map<string, number>,
-    graphData: Array<ClimbGraphData>
-  ) {
-    gradesArray.forEach((gr) => {
-      graphData.push({
-        Grade: gr,
-        Attempts: gradeAttemptMap.get(gr) || 0,
-        Flash: 1,
-      })
-    })
+  function addGradeData(climb: ClimbLog, gradeAttemptMap: Map<string, number>) {
+    const attempts = gradeAttemptMap.get(climb.Grade) || 0
+    gradeAttemptMap.set(climb.Grade, attempts + climb.Attempts)
+  }
+
+  function assembleGraphData(
+    gradesArray: string[],
+    gradeAttemptMap: Map<string, number>
+  ): ClimbGraphData[] {
+    return gradesArray.map((grade) => ({
+      Grade: grade,
+      Attempts: gradeAttemptMap.get(grade) || 0,
+      Flash: 1,
+    }))
   }
 
   return (
     <>
       {climbingData.length > 0 && (
         <Grid container direction="row" sx={{ mt: 5 }}>
-          <GradeGraph climbType="Boulder" graphData={boulderData} />
-          <GradeGraph climbType="Lead" graphData={leadData} />
-          <GradeGraph climbType="Top Rope" graphData={trData} />
+          <GradeGraph climbType="Boulder" graphData={graphData.boulderData} />
+          <GradeGraph climbType="Lead" graphData={graphData.leadData} />
+          <GradeGraph climbType="Top Rope" graphData={graphData.trData} />
         </Grid>
       )}
-      {/* <Grid
+      <Grid
         container
         direction="row"
         style={{
@@ -141,10 +120,10 @@ const GradeGraphWrapper = () => {
             <p>Grade: {climb.Grade}</p>
             <p>Attempts: {climb.Attempts}</p>
             <p>Tick: {climb.Tick}</p>
-            <p>Date: {climb.DateTime.toDateString()}</p>
+            {/* <p>Date: {climb.DateTime.toDateString()}</p> */}
           </div>
         ))}
-      </Grid> */}
+      </Grid>
     </>
   )
 }
