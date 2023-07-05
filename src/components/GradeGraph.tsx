187db972d9e32e97a18ebbfc0ef7fee0c43558ff
Scott Holmes <scott.holmes44@gmail.com>
2023-07-05 14:24:16 -0600

    Created a styles file

--- src/components/GradeGraph.tsx
+++ src/components/GradeGraph.tsx
@@ -1,14 +1,7 @@
 import React from "react"
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  Legend,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts"
+import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts"
 import { ClimbGraphData } from "../static/types"
+import { GraphColors } from "../static/styles"
 import { Typography } from "@mui/material"
 
 export type GradeGraphProps = {
@@ -46,12 +39,11 @@ function GradeGraph({ climbType, graphData }: GradeGraphProps) {
         <XAxis type="number" />
         <YAxis type="category" dataKey="Grade" width={150} tickLine={false} />
         <Tooltip />
-        <Legend />
         <CartesianGrid strokeDasharray="0 0" />
-        <Bar dataKey="Onsight" stackId="a" fill="#16BA6F" />
-        <Bar dataKey="Flash" stackId="a" fill="#FFC733" />
-        <Bar dataKey="Redpoint" stackId="a" fill="#FF4633" />
-        <Bar dataKey="Attempts" stackId="a" fill="#C4C4C4" />
+        <Bar dataKey="Onsight" stackId="a" fill={GraphColors.Onsight} />
+        <Bar dataKey="Flash" stackId="a" fill={GraphColors.Flash} />
+        <Bar dataKey="Redpoint" stackId="a" fill={GraphColors.Redpoint} />
+        <Bar dataKey="Attempts" stackId="a" fill={GraphColors.Attempts} />
       </BarChart>
     </div>
   )
---
+++ src/static/styles.ts
@@ -0,0 +1,6 @@
+export const GraphColors = {
+  Onsight: "#16BA6F",
+  Flash: "#FFC733",
+  Redpoint: "#FF4633",
+  Attempts: "#C4C4C4",
+}
