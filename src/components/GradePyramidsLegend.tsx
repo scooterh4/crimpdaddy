f4d4b4e8664e4b2a2b7d46b5c28644dc02fc952f
Scott Holmes <scott.holmes44@gmail.com>
2023-07-19 12:15:20 -0600

    GradePyramidsLegend - removed card and centered

--- src/components/GradePyramidsLegend.tsx
+++ src/components/GradePyramidsLegend.tsx
@@ -5,43 +5,37 @@ import { GraphColors } from "../static/styles"
 
 const GradePyramidsLegend = () => {
   return (
-    <Card sx={{ display: "inline-flex" }}>
-      <Grid container direction={"row"} sx={{ display: "inline-flex" }}>
-        <Grid
-          item
-          sm={3}
-          xs={6}
-          marginRight={6}
-          sx={{ display: "inline-flex" }}
-        >
+    <>
+      {/* <Card sx={{ display: "inline-flex" }}> */}
+      <Grid
+        container
+        justifyContent={"center"}
+        direction={"row"}
+        spacing={2}
+        sx={{ display: "inline-flex" }}
+      >
+        <Grid item sx={{ display: "inline-flex" }}>
           <Square sx={{ color: GraphColors.Onsight }} />
           <Typography>Onsight</Typography>
         </Grid>
 
-        <Grid item sm={3} xs={6} sx={{ display: "inline-flex" }}>
+        <Grid item sx={{ display: "inline-flex" }}>
           <Square sx={{ color: GraphColors.Flash }} />
           <Typography>Flash</Typography>
         </Grid>
-      </Grid>
 
-      <Grid container direction={"row"} sx={{ display: "inline-flex" }}>
-        <Grid
-          item
-          sm={3}
-          xs={6}
-          marginRight={6}
-          sx={{ display: "inline-flex" }}
-        >
+        <Grid item sx={{ display: "inline-flex" }}>
           <Square sx={{ color: GraphColors.Redpoint }} />
           <Typography>Redpoint</Typography>
         </Grid>
 
-        <Grid item sm={3} xs={6} sx={{ display: "inline-flex" }}>
+        <Grid item sx={{ display: "inline-flex" }}>
           <Square sx={{ color: GraphColors.Attempts }} />
           <Typography>Other</Typography>
         </Grid>
       </Grid>
-    </Card>
+      {/* </Card> */}
+    </>
   )
 }
 
