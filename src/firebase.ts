31bdca1d27bfc71394170b8b7576a9618eaddcb6
Scott Holmes <scott.holmes44@gmail.com>
2023-08-02 16:18:42 -0700

    setup prod and dev env's

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-// TODO need to setup get these variables out of the env file
-// i.e. process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 const app = initializeApp({
-  apiKey: "AIzaSyBWhsaC6888q3kV2oxDIqtETpYjJ_va8IY",
-  authDomain: "crimpdaddy-db2af.firebaseapp.com",
-  projectId: "crimpdaddy-db2af",
-  storageBucket: "crimpdaddy-db2af.appspot.com",
-  messagingSenderId: "908883030660",
-  appId: "1:908883030660:web:e1b52563a8985c17d588d1",
-  measurementId: "G-2MR09LRHZW",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 })
 
 const authentication = getAuth(app)
